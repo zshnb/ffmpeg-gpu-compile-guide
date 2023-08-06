d2cc0af0831aed9dd789ae9495b0614fe97254e6
SlickZheng <slick.zheng@opus.pro>
2023-08-06 14:57:11 +0800

    add -y in apt install

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 cd nv-codec-headers && sudo make install && cd ../
 echo 'installed ffnvcodec'
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
-sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+sudo apt-get install -y build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
 echo 'installed basic dependencies'
 
 apt-get install -y frei0r-plugins-dev
