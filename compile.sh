7dc0593429524576ff9a274c89e58121a2afd163
SlickZheng <slick.zheng@opus.pro>
2023-08-06 20:23:48 +0800

    fix shell script

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@
 mkdir build-ffmpeg
 cd build-ffmpeg
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-cd nv-codec-headers && sudo make install && cd ../
+cd nv-codec-headers && make install && cd ../
 echo 'installed ffnvcodec'
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
-sudo apt-get install -y build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev libdrm-dev libass-dev libopus-dev libmp3lame-dev libva-dev libfdk-aac-dev
+apt-get install -y build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev libdrm-dev libass-dev libopus-dev libmp3lame-dev libva-dev libfdk-aac-dev
 echo 'installed basic dependencies'
 
 apt-get install -y libavcodec-dev libavformat-dev libavutil-dev
@@ -22,7 +22,6 @@ apt-get install -y libgsm1-dev
 apt-get install -y libmysofa-dev
 apt-get install -y flite1-dev
 apt-get install -y libcodec2-dev
-apt-get install -y libavcodec-dev libavformat-dev libavutil-dev
 apt-get install -y libopenjp2-7-dev
 apt-get install -y libopenmpt-dev
 apt-get install -y librsvg2-dev
@@ -49,30 +48,31 @@ apt-get install -y libcdparanoia-dev
 apt-get install -y libcdio-dev libcdio-paranoia-dev
 apt-get install -y libsdl2-dev
 apt-get install -y libtheora-dev
+apt-get install -y libgme-dev
 echo 'installed extra dependencies'
 
 mkdir aom
 cd aom
 git clone https://aomedia.googlesource.com/aom
-cmake ./aom && make && sudo make install
+cmake ./aom && make && make install
 echo 'installed aom'
 cd ..
 
 git clone https://github.com/sekrit-twc/zimg.git --recursive
 cd zimg
-sh autogen.sh && ./configure && make
+sh autogen.sh && ./configure && make install
 echo 'installed zimg'
 cd ..
 
 ldconfig
 
-wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
-sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.3.250-focal.list https://packages.lunarg.com/vulkan/1.3.250/lunarg-vulkan-1.3.250-focal.list
-sudo apt update
-sudo apt install -y vulkan-sdk
+wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
+wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.3.250-focal.list https://packages.lunarg.com/vulkan/1.3.250/lunarg-vulkan-1.3.250-focal.list
+apt update
+apt install -y vulkan-sdk
 echo 'installed vulkan'
 
-sudo apt-get -y install python3-pip
+apt-get -y install python3-pip
 pip3 install meson ninja
 git clone --recursive https://code.videolan.org/videolan/libplacebo
 cd libplacebo
@@ -87,6 +87,7 @@ if [ ! -x conf.sh ]; then
   chmod a+x conf.sh
 fi
 cp conf.sh ffmpeg/
+cd ffmpeg
 ./conf.sh
 make -j 8
 make install
