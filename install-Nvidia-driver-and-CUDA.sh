70cc5b18f26f2cb71bdfeb1fe6d62369d99a68f7
jingjingxyk <zonghengbaihe521@qq.com>
2023-08-07 17:05:53 +0800

    add auto build script

--- install-Nvidia-driver-and-CUDA.sh
+++ install-Nvidia-driver-and-CUDA.sh
@@ -1,15 +1,16 @@
 
 
-  wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-  mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-  wget -q https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-  dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-  rm cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-  apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
 
-  wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
-  dpkg -i cuda-keyring_1.0-1_all.deb
-  rm cuda-keyring_1.0-1_all.deb
+wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget -q https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+rm cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+
+wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
+rm cuda-keyring_1.0-1_all.deb
 
 
 cat >/etc/apt/preferences.d/cuda-11.6-pin-1000.pref <<'EOF'
